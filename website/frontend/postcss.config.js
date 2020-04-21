837de775441c6e4693ba524c99e1ffb9debd7922
Thang Pham <phamducthang1234@gmail.com>
2020-04-21 01:10:37 -0400

    config purgecss

--- website/frontend/postcss.config.js
+++ website/frontend/postcss.config.js
@@ -1,13 +1,8 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-
 module.exports = {
     plugins: [
         // ...
         require("tailwindcss"),
         require("autoprefixer"),
-        ...process.env.NODE_ENV === 'production'
-          ? [purgecss]
-          : []
         // ...
     ],
 };
